2fb257839b83ef6276adcc6621d1cde7af54347c
Rafael Andrade <rafael.io@outlook.com>
2025-06-22 22:01:53 -0300

    Muda caminho relativo da config database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Movie } from 'src/modules/movies/models/movie.model';
+import { Movie } from '../modules/movies/models/movie.model';
 
 @Module({
   imports: [
